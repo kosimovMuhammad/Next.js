156fa903a4bf04bf58279afc8e4101f45ffe9572
kosimovMuhammad <mmgosimov@gmail.com>
2026-07-13 23:25:42 +0500

    Revert "Day 6"

--- app/[locale]/demos/parallel/@analytics/page.tsx
+++
@@ -1,6 +0,0 @@
-import { getTranslations } from "next-intl/server";
-
-export default async function AnalyticsSlotPage() {
-  const t = await getTranslations("ParallelDemo");
-  return <p className="text-sm">{t("analyticsOverview")}</p>;
-}
--- app/[locale]/demos/parallel/@team/settings/page.tsx
+++
@@ -1,6 +0,0 @@
-import { getTranslations } from "next-intl/server";
-
-export default async function TeamSettingsPage() {
-  const t = await getTranslations("ParallelDemo");
-  return <p className="text-sm">{t("teamSettings")}</p>;
-}
